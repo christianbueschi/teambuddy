29e6ae3ee40a676b4766080a2ed3440b55ae7db1
Christian <christianbueschi@gmail.com>
2014-11-30 14:02:49 +0100

    Initial commit

---
+++ www/mod-events/eventsCtrl.js
@@ -0,0 +1,13 @@
+var app = angular.module('tb.events', []);
+
+app.controller('EventsCtrl', function($scope) {
+  
+  $scope.events = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+});
---
+++ www/mod-nav/navCtrl.js
@@ -0,0 +1,6 @@
+var app = angular.module('tb.nav', []);
+
+app.controller('NavCtrl', function($scope, $ionicModal, $timeout) {
+
+  
+});
